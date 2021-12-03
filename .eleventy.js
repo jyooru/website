75aef1ee4df1f6948b8dda4c49635839902b5cd7
Joel <joel@joel.tokyo>
2021-12-03 11:58:45 +1000

    feat: enable 404 page for eleventy --serve

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,19 @@
+const fs = require("fs");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function (err, bs) {
+        bs.addMiddleware("*", (req, res) => {
+          // enable 404 page for eleventy --serve
+          const content_404 = fs.readFileSync("_site/404.html");
+          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+          res.write(content_404);
+          res.end();
+        });
+      },
+    },
+  });
+
   eleventyConfig.addWatchTarget("./assets/sass/");
 };
