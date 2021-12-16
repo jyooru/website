d06597845f086e0b24f27c60415538748850ba91
Joel <joel@joel.tokyo>
2021-12-17 00:10:35 +1000

    feat: fully reproducible devShell

--- flake.nix
+++ flake.nix
@@ -13,56 +13,24 @@
 
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
-
-        nodePackages = with pkgs; stdenv.mkDerivation {
-          name = "node-packages";
-
-          src = lib.cleanSourceWith {
-            filter = (path: type:
-              type == "regular" &&
-              builtins.any (x: baseNameOf path == x) [ "package.json" "package-lock.json" ]
-            );
-            src = ./.;
-          };
-
-          nativeBuildInputs = [ pkgs.nodePackages.node2nix ];
-
-          buildPhase = ''
-            node2nix -d -l package-lock.json
-          '';
-
-          installPhase = ''
-            mkdir $out
-            cp ./* $out
-          '';
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ dotfiles.overlays.node-packages ];
         };
-        node = import nodePackages { inherit pkgs system; inherit (pkgs) nodejs; };
-        nodeScript = (script: pkgs.writeShellScriptBin
-          "node-script-${script}"
-          (builtins.replaceStrings
-            [ "exec ${pkgs.stdenv.shell}" ]
-            [ "exec ${pkgs.nodejs}/bin/npm run ${script}" ]
-            (builtins.readFile (toString node.shell + "/bin/shell"))
-          )
-        );
 
-        inherit (node.args) name version;
+        packages = (with pkgs; [ git ] ++ (with nodePackages; [
+          nodePackages."normalize.css"
+          nodePackages."@11ty/eleventy"
+          nodePackages."@11ty/eleventy-cache-assets"
+          html-minifier
+          sass
+          simple-git
+          nodePackages."terminal.css"
+          ttf2woff2
+        ]));
       in
-      with pkgs;
       rec {
-        apps = builtins.listToAttrs (map (name: { inherit name; value = nodeScript name; }) [
-          "build"
-          "serve"
-          "watch"
-        ]);
-        defaultApp = apps.serve;
-
-        devShell = node.shell;
-
-        packages = {
-          inherit nodePackages;
-        };
+        devShell = pkgs.mkShell { inherit packages; };
       }
     );
 }
