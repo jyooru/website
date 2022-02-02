a8c52b1c97f3cbf5a8025d7434d0b12c87f526d9
Joel <joel@joel.tokyo>
2022-02-02 14:08:10 +1000

    feat!: cleanup
    - remove unreproducible website
      - don't need the unreproducible version
    - refactor flake
    - remove apps i don't need

--- src/_data/env.js
+++ src/_data/env.js
@@ -2,6 +2,5 @@ module.exports = function () {
   return {
     nodePath: process.env.NODE_PATH,
     nodeModules: JSON.parse(process.env.NODE_MODULES),
-    reproducibleBuild: process.env.REPRODUCIBLE_BUILD == "1",
   };
 };
